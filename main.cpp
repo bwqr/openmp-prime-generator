fa5fa223688929382cd780dc85d584f448e3617e
bwqr <ruzgardeniz.08@hotmail.com>
2020-04-08 00:08:23 +0300

    another implementation

--- main.cpp
+++ main.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 #include <vector>
 #include <cmath>
-#include <cstring>
+#include <omp.h>
 
 //#define NUM_PER_TASK 2048
 
@@ -9,11 +9,11 @@ typedef int32_t num;
 
 static num NUM_PER_TASK;
 
-static num pointer = 0;
+void mergeSortedVectors(std::vector<num> *dest, std::vector<std::vector<num>> &sortedVectors);
 
 void primeRecursive(num upper_bound, num min_bound, std::vector<num> *primes);
 
-void primeGenerator(num start, num end, const std::vector<num> &primes, std::vector<num> *foundPrimes);
+void primeGenerator(num start, num end, const std::vector<num> &primes, std::vector<std::vector<num>> *foundPrimes);
 
 num numberOfPrimes(const num &number);
 
@@ -25,58 +25,49 @@ int main(int argc, char **argv) {
     }
 
     num N = std::stoi(argv[1]);
-    NUM_PER_TASK = 100;
+    NUM_PER_TASK = 1000;
     std::vector<num> primes;
-    auto size = static_cast<unsigned long>(numberOfPrimes(N)) * 2;
 
     if (N < 2) {
         goto exit;
     }
-    primes.resize(size);
-    primes[pointer++] = 2;
+
+    primes.reserve(static_cast<unsigned long>(numberOfPrimes(N)));
+    primes.push_back(2);
 
     if (N == 2) {
         goto print;
     }
-    primes[pointer++] = 3;
+    primes.push_back(3);
     primeRecursive(N, 10, &primes);
+
     print:
 //    std::cout << primes.size() << std::endl;
-    for (size_t i = 0; i < pointer; i++) std::cout << primes[i] << std::endl;
+    for (size_t i = 0; i < primes.size(); i++) std::cout << primes[i] << std::endl;
     exit:
     return 0;
 }
 
 void primeRecursive(num upper_bound, num min_bound, std::vector<num> *primes) {
-    auto &p = *primes;
+
+    static int num_threads = omp_get_max_threads();
+
     if (upper_bound < min_bound) {
-        std::vector<num> foundPrimes;
-        primeGenerator(3, upper_bound, p, &foundPrimes);
-        for (const auto &num: foundPrimes) {
-            p[pointer++] = num;
-        }
+        std::vector<std::vector<num>> foundPrimes(num_threads);
+
+        primeGenerator(3, upper_bound, *primes, &foundPrimes);
+
+        mergeSortedVectors(primes, foundPrimes);
     } else {
-        num lower_bound = static_cast<num>(std::sqrt(upper_bound));
+        num lower_bound = static_cast<num>(std::ceil(std::sqrt(upper_bound)));
 
         primeRecursive(lower_bound, min_bound, primes);
 
-        num size = std::ceil(static_cast<float>(upper_bound - lower_bound) / NUM_PER_TASK);
+        std::vector<std::vector<num>> foundPrimes(num_threads);
 
-        std::vector<std::vector<num>> foundPrimes(size);
+        primeGenerator(lower_bound + 1, upper_bound, *primes, &foundPrimes);
 
-#pragma omp parallel for default(none) shared(lower_bound, upper_bound, foundPrimes, p, NUM_PER_TASK)
-        for (num i = lower_bound + 1; i < upper_bound; i += NUM_PER_TASK) {
-            primeGenerator(i, std::min(i + NUM_PER_TASK - 1, upper_bound), p,
-                           &foundPrimes[(i - lower_bound) / NUM_PER_TASK]);
-        }
-
-        for (const auto &fprime: foundPrimes) {
-            memcpy(&p[pointer], fprime.data(), fprime.size() * sizeof(num));
-            pointer += fprime.size();
-//            for (const auto &prime: fprime) {
-//                p[pointer++] = prime;
-//            }
-        }
+        mergeSortedVectors(primes, foundPrimes);
     }
 }
 
@@ -84,49 +75,98 @@ num numberOfPrimes(const num &number) {
     return static_cast<num>(static_cast<float>(number) / std::log(number - 1));
 }
 
-void primeGenerator(num start, num end, const std::vector<num> &primes, std::vector<num> *foundPrimes) {
+void primeGenerator(num start, num end, const std::vector<num> &primes, std::vector<std::vector<num>> *foundPrimes) {
     int j;
-    int k;
     int n;
-    int quo, rem;
 
-    auto &p = *foundPrimes;
-    p.reserve(static_cast<unsigned long>(std::max<num>(numberOfPrimes(end) - numberOfPrimes(start), 1)));
+    for (auto &f: *foundPrimes) {
+        f.reserve(static_cast<unsigned long>(std::max<num>(numberOfPrimes(end) - numberOfPrimes(start), 1)) /
+                  foundPrimes->size());
+    }
 
     P1:
     n = start % 2 == 0 ? start + 1 : start;
-    goto P5;
 
-    P2:
-    p.push_back(n);
+#pragma omp parallel for default(none) shared(foundPrimes, primes, end, n)
+    for (num i = n; i <= end; i += 2) {
+        int k;
+        int quo, rem;
 
-    P3:
-    n = n + 2;
+        auto index = omp_get_thread_num();
 
-    if (n > end) {
-        return;
+        goto P5;
+
+        P2:
+        (*foundPrimes)[index].push_back(i);
+
+        P3:
+//        n = n + 2;
+        continue;
+//        if (n > end) {
+//            return;
+//        }
+        P5:
+        k = 1;
+
+        P6:
+        if (k >= primes.size()) {
+            goto P2;
+        }
+
+        quo = i / primes[k];
+        rem = i % primes[k];
+
+        if (rem == 0) {
+            goto P3;
+        }
+
+        P7:
+        if (quo <= primes[k]) {
+            goto P2;
+        }
+
+        P8:
+        k = k + 1;
+        goto P6;
     }
-    P5:
-    k = 1;
+}
 
-    P6:
-    if (k >= pointer) {
-        goto P2;
+void mergeSortedVectors(std::vector<num> *dest, std::vector<std::vector<num>> &sortedVectors) {
+    size_t currentPointerIndex = 0;
+    std::vector<std::vector<num>::iterator> pointers;
+
+    for (size_t i = 0, j = 0; i < sortedVectors.size(); i++) {
+        if(sortedVectors[i - j].size() > 0) {
+            pointers.push_back(sortedVectors[i - j].begin());
+        } else {
+            sortedVectors.erase(sortedVectors.begin() + i - j);
+            j++;
+        }
     }
 
-    quo = n / primes[k];
-    rem = n % primes[k];
+    while (!pointers.empty()) {
+        if (pointers[currentPointerIndex] == sortedVectors[currentPointerIndex].end()) {
+            if (pointers.size() <= 1) {
+                break;
+            }
 
-    if (rem == 0) {
-        goto P3;
+            sortedVectors.erase(sortedVectors.begin() + currentPointerIndex);
+            pointers.erase(pointers.begin() + currentPointerIndex);
+            currentPointerIndex %= pointers.size();
+            continue;
+        }
+
+        num min = *pointers[0];
+        std::vector<num>::iterator it = pointers[0];
+        for (size_t i = 1; i < pointers.size(); i++) {
+            if (*pointers[i] < min) {
+                min = *pointers[i];
+                it = pointers[i];
+                currentPointerIndex = i;
+            }
+        }
+
+        dest->push_back(*pointers[currentPointerIndex]);
+        pointers[currentPointerIndex]++;
     }
-
-    P7:
-    if (quo <= primes[k]) {
-        goto P2;
-    }
-
-    P8:
-    k = k + 1;
-    goto P6;
 }
